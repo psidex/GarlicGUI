26e671b7400c182ede81f6b4fd8bd84eb72216b7
Simon <simjenner3@gmail.com>
2018-01-25 22:19:34 +0000

    Inital file commit

---
+++ src/main/view/mainController.java
@@ -0,0 +1,12 @@
+package main.view;
+
+// import javafx.fxml.FXML;
+
+public class mainController {
+
+    // called by the FXML loader after the labels declared above are injected:
+    public void initialize() {
+        // do initialization and configuration work...
+    }
+
+}
